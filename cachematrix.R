979c772df3dd45a5328d94071848cb8cf8602d33
jonvsm <jonsm@live.com>
2017-01-07 11:10:21 -0800

    Updated comments in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-##These functions together illustrate some key aspects of lexical scoping in R
+#makeCacheMatrix and cachsolve together illustrate some key aspects of lexical scoping in R
 
 ##makeCacheMatrix takes a matrix as input and defines a set of functions - set, 
 ##get, setinverse, getinverse for acting on that matrix
@@ -17,8 +17,11 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 }   
 
-## cacheSolve calls functions defined in makeCacheMatrix to check if the inverse
-##of the matrix is cached and, if it is not, call getinverse and return the inverse
+## cacheSolve calls functions defined in makeCacheMatrix
+##It calculates the inverse of the special "matrix" created with the function above. 
+##It first checks to see if the inverse has already been calculated and has not changed.
+##If so, it gets the inverse matrix from the cache and skips the computation. 
+##Otherwise, it calculates the inverse of the matrix and saves the inverse in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
